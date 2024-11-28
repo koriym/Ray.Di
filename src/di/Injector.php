00ee52b190f7b5e89902ae97f38b1f90a03db4f3
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-29 01:56:12 +0900

    Ensure tmp directory is writable in Injector setup.
    Add a check in the Injector class to verify that the temporary directory is writable before proceeding with the class directory initialization. Introduced a new exception, DirectoryNotWritable, to handle cases where the directory is not writable, enhancing error handling and code robustness.

--- src/di/Injector.php
+++ src/di/Injector.php
@@ -6,11 +6,13 @@ namespace Ray\Di;
 
 use Doctrine\Common\Annotations\AnnotationException;
 use Ray\Aop\Compiler;
+use Ray\Di\Exception\DirectoryNotWritable;
 use Ray\Di\Exception\Untargeted;
 
 use function assert;
 use function file_exists;
 use function is_dir;
+use function is_writable;
 use function spl_autoload_register;
 use function sprintf;
 use function str_replace;
@@ -32,6 +34,10 @@ class Injector implements InjectorInterface
     {
         /** @var non-empty-string $classDir */
         $classDir = is_dir($tmpDir) ? $tmpDir : sys_get_temp_dir();
+        if (! is_writable($classDir)) {
+            throw new DirectoryNotWritable($classDir); // @CodeCoverageIgnore
+        }
+
         $this->classDir = $classDir;
         $this->container = (new ContainerFactory())($module, $this->classDir);
         // Bind injector (built-in bindings)

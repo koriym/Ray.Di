228494c68d9deb32830c92d36b9a78e07b2e1d17
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-08-29 18:24:11 +0900

    Keep MethodInvocation per-coroutine LIFO; clear after interception

--- src/di/MethodInvocationProvider.php
+++ src/di/MethodInvocationProvider.php
@@ -7,25 +7,49 @@ namespace Ray\Di;
 use Ray\Aop\MethodInvocation;
 use Ray\Di\Exception\MethodInvocationNotAvailable;
 
-/** @implements ProviderInterface<MethodInvocation> */
+use function array_key_last;
+use function array_pop;
+use function count;
+
+/**
+ * Per-coroutine LIFO stack of in-flight method invocations
+ *
+ * A completed or abandoned interception must not leave the finished invocation
+ * resolvable, and a nested interception must restore the enclosing one.
+ *
+ * @implements ProviderInterface<MethodInvocation>
+ */
 final class MethodInvocationProvider implements ProviderInterface
 {
-    /** @var ?MethodInvocation<object> */
-    private ?MethodInvocation $invocation = null;
+    /** @var array<int, list<MethodInvocation<object>>> */
+    private array $invocations = [];
 
     /** @param MethodInvocation<object> $invocation */
     public function set(MethodInvocation $invocation): void
     {
-        $this->invocation = $invocation;
+        $cid = CoroutineContext::id();
+        $this->invocations[$cid][] = $invocation;
+    }
+
+    public function pop(): void
+    {
+        $cid = CoroutineContext::id();
+        array_pop($this->invocations[$cid]);
+        if (count($this->invocations[$cid]) === 0) {
+            unset($this->invocations[$cid]);
+        }
     }
 
     /** @return MethodInvocation<object> */
     public function get(): MethodInvocation
     {
-        if ($this->invocation === null) {
+        $stack = $this->invocations[CoroutineContext::id()] ?? null;
+        if ($stack === null) {
             throw new MethodInvocationNotAvailable();
         }
 
-        return $this->invocation;
+        /** @psalm-suppress PossiblyNullArrayOffset -- $stack is the non-null list guarded above */
+
+        return $stack[array_key_last($stack)]; // @phpstan-ignore offsetAccess.notFound
     }
 }
